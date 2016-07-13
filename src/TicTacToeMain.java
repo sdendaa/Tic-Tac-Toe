067888bf33b31cd8c13ff69dc87b275b9afedfbd
sdendaa <sdendaa@gmail.com>
2016-07-13 00:58:27 -0700

    add the comment in the main class

--- src/TicTacToeMain.java
+++ src/TicTacToeMain.java
@@ -3,6 +3,7 @@ import javax.swing.JFrame;
 public class TicTacToeMain {
 
 	public static void main(String[] args) {
+		//this is how  we call the JFrame class in main class
 		JFrame TicTacToeFrame = new TicTacToeFrame();
 		TicTacToeFrame.setTitle("This is my Tic Tac Toe Game");
 		TicTacToeFrame.setSize(600, 600);
